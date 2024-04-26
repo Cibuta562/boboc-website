22952f9e3685a71e2200266d7516252b1dbc692c
robertandreiboboc <andreiboboccibo@gmail.com>
2024-04-26 16:25:30 +0300

    credits margin

--- src/pages/links.js
+++ src/pages/links.js
@@ -21,12 +21,12 @@ function Links() {
                 <p className="function-name">web developer</p>
                 <div className="links-list">
                     <div className="social-icons">
-                        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FaLinkedin style={{ color: 'black', fontSize: '30px' }} /></a>
+                        <a href="https://www.linkedin.com/in/boboc-robert-andrei-a2b21a175/" target="_blank" rel="noopener noreferrer"><FaLinkedin style={{ color: 'black', fontSize: '30px' }} /></a>
                         <a href="https://api.whatsapp.com/send?phone=40723344599" target="_blank" rel="noopener noreferrer"><FaWhatsapp style={{ color: 'black', fontSize: '30px' }} /></a>
-                        <a href="mailto:your-email@example.com"><FaEnvelope style={{ color: 'black', fontSize: '30px' }} /></a>
+                        <a href="mailto:andreiboboccibo@gmail.com"><FaEnvelope style={{ color: 'black', fontSize: '30px' }} /></a>
                     </div>
                     <div className="links-padding">
-                        <a className="links-p">generationsalez.com</a>
+                        <a href="https://generationsalez.com" target="_blank" className="links-p">generationsalez.com</a>
                     </div>
                     <div className="links-padding">
                         <a className="links-p">Portfolio</a>
@@ -35,10 +35,10 @@ function Links() {
                         <a className="links-p">Contact Form</a>
                     </div>
                     <div className="links-padding">
-                        <a className="links-p">Github</a>
+                        <a href="https://github.com/Cibuta562" target="_blank" className="links-p">Github</a>
                     </div>
                     <div className="links-padding">
-                        <a className="links-p">Puzzle Studios</a>
+                        <a href="https://linktr.ee/puzzle_studio" target="_blank" className="links-p">Puzzle Studios</a>
                     </div>
                     <div className="credits">
                         <a className="links-down">© generation salez 2024</a>
